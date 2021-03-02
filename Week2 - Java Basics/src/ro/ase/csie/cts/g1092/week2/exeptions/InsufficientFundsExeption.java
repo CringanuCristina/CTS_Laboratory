313efc36881c089c02dddcc3ab37f021f15068a4
CringanuCristina <61270518+CringanuCristina@users.noreply.github.com>
2021-03-02 10:38:19 +0200

    seminar 2 before break

---
+++ Week2 - Java Basics/src/ro/ase/csie/cts/g1092/week2/exeptions/InsufficientFundsExeption.java
@@ -0,0 +1,10 @@
+package ro.ase.csie.cts.g1092.week2.exeptions;
+
+public class InsufficientFundsExeption extends Exception {
+	public InsufficientFundsExeption(String message) {
+		super(message);
+	}
+	public InsufficientFundsExeption() {
+		
+	}
+}
