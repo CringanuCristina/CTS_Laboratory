c32e9269ba1d76c205607ede56b7f09cda6b6efb
CringanuCristina <61270518+CringanuCristina@users.noreply.github.com>
2021-03-11 11:29:32 +0200

    course but not complete

--- Course3/src/ro/ase/csie/cts/entities/Student.java
+++ Course3/src/ro/ase/csie/cts/entities/Student.java
@@ -2,4 +2,39 @@ package ro.ase.csie.cts.entities;
 
 public class Student {
 
+	boolean hasScolarship;
+	boolean isEnroled;
+	int age;
+	
+	void payTuition() {} //depends on accounting
+	void takeOOPExam() {} //depends on the prof
+	void saveTODB() {} //depends on DB admin
+	
+	void incrementAge() {
+		if(this.age<20) {
+			this.age +=1;
+		}
+		else {
+			this.age+=2;
+		}
+		
+		this.age = (this.age<20)? (this.age+=1): (this.age +=2);
+	}
+	
+	String getInfo() {
+		String message;
+		if(!this.hasScolarship) {
+			return "no scolarship";
+		}
+		if(this.hasScolarship) {
+			return "the student has a scolarship";
+		}
+		else {
+			return "no scolarship";
+		}
+	}
+	
+	public int aSimpleFunction() {
+		return 10;
+	}
 }
