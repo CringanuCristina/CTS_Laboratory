3411f354b8289442971654f554e6af0d61db2dd0
CringanuCristina <61270518+CringanuCristina@users.noreply.github.com>
2021-04-06 11:28:31 +0300

    Facade example

---
+++ week 7 - Factory/src/ro/ase/csie/cts/g1092/dp/facade/PlayerProfile.java
@@ -0,0 +1,11 @@
+package ro.ase.csie.cts.g1092.dp.facade;
+
+public class PlayerProfile {
+ public void login() {
+	 
+ }
+ 
+ public void getUserSettings() {
+	 
+ }
+}
---
+++ week 7 - Factory/src/ro/ase/csie/cts/g1092/dp/facade/SuperHero.java
@@ -0,0 +1,15 @@
+package ro.ase.csie.cts.g1092.dp.facade;
+
+public class SuperHero {
+	String name;
+	Helmet helmet;
+	SuperPower superpower;
+	
+	public SuperHero(String name, Helmet helmet, SuperPower superpower) {
+		super();
+		this.name = name;
+		this.helmet = helmet;
+		this.superpower = superpower;
+	}
+	
+}
